ebd7656bf41b3ec14435423edcac8794506cd335
Ramya.U <ramyayadav2016@gmail.com>
2021-03-10 15:47:41 +0530

    [Ramya] Add . Added values and variables

--- Fundamentals Part-1/script.js
+++ Fundamentals Part-1/script.js
@@ -4,3 +4,15 @@
 
     40+8+23-10;
     console.log(40 + 8 + 23 - 10);
+
+    console.log("Ramyaaa");
+    console.log(23);
+
+    let firstname = "Rumz";
+    
+    console.log(firstname);
+    console.log(firstname);
+    console.log(firstname);
+    console.log(firstname);
+    
+    
